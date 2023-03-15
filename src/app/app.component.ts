0bc1e1c6301d1a2a546e4b317e673ecad638bf28
barry <ousmanebarry994@gmail.com>
2023-03-15 21:44:34 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
+import { DatePipe, formatDate } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { CampagneService } from './campagne.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [DatePipe],
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'testTechnique2';
+  title = 'testTechnique1';
+  searchText = "";
+  searchBybrand:number =1;
+  listBrands: Array<any> = [];
+  listOfCampagnes: Array<any> = [];
+  campaignForm!: FormGroup;
+  listMediasBis: Array<any> = [];
+  listMedias: Array<any> = [];
+
+  
+  constructor(private campagneService: CampagneService, private formBuilder: FormBuilder,private datePipe: DatePipe
+    ){
+    this.getCampagnes();
+    this.getlistMedias();
+  }
+
+  ngOnInit() {
+        this.campaignForm = this.formBuilder.group({
+            brand: ['', Validators.required],
+            campaignName: ['', Validators.required],
+            decisionDeadline: ['',Validators.required],
+
+        });
+      }
+  getCampagnes(){
+    this.listOfCampagnes = this.campagneService.getpayloadRmp()[0].requests;
+    this.listBrands = this.campagneService.getBrands();
+  }
+
+  getlistMedias(){
+    this.listMedias = this.campagneService.getmyMedias();
+    this.listMediasBis = this.campagneService.getmyMediasBis();
+  }
+
+  fitreCompaign(brandId:number){
+    this.listOfCampagnes = this.listOfCampagnes.filter(
+      (word:any) => word.brand.brandId == brandId
+    );
+
+  }
+
+  getChecketMedia(data:any){
+    this.listMedias.forEach((element:any) => {
+      data.media.forEach((element2:any) => {
+        if(element.value==element2.value){
+           element.isChecked =true;
+        }
+      })
+    });
+
+    this.listMediasBis.forEach((element3:any) => {
+      data.media.forEach((element4:any) => {
+        if(element3.value==element4.value){
+          element3.isChecked =true;
+        }
+      })
+    });
+  }
+      showDetailCompagne(data:any){
+        this.getChecketMedia(data);
+        this.campaignForm.patchValue({
+          brand: data.brand.name,
+          campaignName:data.campaignName,
+          media:data.media,
+          decisionDeadline: formatDate(data?.decisionDeadline, 'yyyy-MM-dd', 'en')
+        });
+      }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    Ng2SearchPipeModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
